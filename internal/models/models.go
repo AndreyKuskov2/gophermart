e033ef332f7a50c0cdadc6cbba85991b4f7c60d5
Andrey Kuskov <andrey.kuskov.05@gmail.com>
2025-06-16 15:22:40 +0300

    update api

--- internal/models/models.go
+++ internal/models/models.go
@@ -25,7 +25,7 @@ type Orders struct {
 	OrderID    int       `json:"order_id"`
 	Number     string    `json:"number"`
 	Status     string    `json:"status"`
-	Accrual    int       `json:"accrual"`
+	Accrual    float32   `json:"accrual"`
 	UploadedAt time.Time `json:"uploaded_at"`
 	UserID     int       `json:"user_id"`
 }
